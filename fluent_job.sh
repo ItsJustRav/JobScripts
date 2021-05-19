93a3e755bd63d211295811b7776baa7f4938f726
Rav <r.t.b.ranaweera@northumbria.ac.uk>
2021-05-19 13:26:07 +0100

    working jobscript
    changed to \n line breaks and modified to ANSYS2021R1 with journal file to load case, run and save data.

--- fluent_job.sh
+++ fluent_job.sh
@@ -12,8 +12,8 @@
 #SBATCH --error=err_%job-name.log      ## Error log
 #SBATCH --output=out_%job-name.log     ## Output log
 #SBATCH --mail-user=email@uni.ac.uk    ## E-mail for notifications
-#SBATCH --mail-type=<ALL>              ## Types of notification for e-mails
-#SBATCH --partition=que                ## Job que to submit
+#SBATCH --mail-type=ALL                ## Types of notification for e-mails
+#SBATCH --partition=24hour             ## Job que to submit <24hour, 48hou, 120hour>
 ####################################
 
 nodes=$SLURM_JOB_NUM_NODES            ## Number of nodes
@@ -22,16 +22,16 @@ cores=$SLURM_CPUS_ON_NODE             ## Number of cores
 # Load Modules and setup
 ## Commented out due to these being included in .bashrc file for load at login.
 ## module purge  ## Remove all modules
-## module add openmpi/intel-opa/gcc-hfi/64/1.10.4  ## Load dedicated MPI module
-## module add gcc/5.2.0 ## Load GCC module
-## module add slurm/15.08.6 ## Load the SLURM module
-## module add ANSYS/18.0  ## Load the ANSYS module
+module add openmpi/intel-opa/gcc-hfi/64/1.10.4  ## Load dedicated MPI module
+module add gcc/5.2.0 ## Load GCC module
+# module add slurm/15.08.6 ## Load the SLURM module
+module load ANSYS/2021R1  ## Load the ANSYS module
 
 # Case Details
-JOURNALFILE= ## fluent.journal  ## Journal file name
-FLUENT = ## /ansys_inc/v190/fluent/bin/fluent ## Module path
+# fluent.journal  ## Journal file name
+JOURNALFILE= ab_iso_kwsst/run_case.journal
 
 # Execute Solver
-$FLUENT 3d -g -slurm -t$NPROCS -mpi=openmpi -i $JOURNALFILE > fluent.log
+fluent 3d -g -slurm -t$NPROCS -mpi=openmpi -i $JOURNALFILE > fluent.log
 
 
