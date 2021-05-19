09dcff047e252d868ed0babe5fc54f11008a885d
Ravindu <r.t.b.ranaweera@northumbria.ac.uk>
2021-05-19 01:17:22 +0100

    modify comments format and partitions

--- fluent_job.sh
+++ fluent_job.sh
@@ -1,34 +1,59 @@
 #!/bin/bash
 
-## Commands have been commented out with "##". Un-comment as needed.
-## Alternatively, copy the needed to main run script.
+# Commands have been commented out with "##". Un-comment as needed.
+# Alternatively, copy the needed to main run script.
 
-####################################
+# ###################################
 # Set up submit to ...
-#SBATCH --job-name="JobName"           ## Job name
-#SBATCH --time=12:00:00                ## Time limit hrs:mm:ss
-#SBATCH --nodes=2                      ## Number of nodes
-#SBATCH --ntasks-per-node=24           ## Number of tasks per per node
-#SBATCH --error=err_%job-name.log      ## Error log
-#SBATCH --output=out_%job-name.log     ## Output log
-#SBATCH --mail-user=email@uni.ac.uk    ## E-mail for notifications
-#SBATCH --mail-type=<ALL>              ## Types of notification for e-mails
-#SBATCH --partition=que                ## Job que to submit
-####################################
+# Job name
+#SBATCH --job-name="JobName"  
 
-nodes=$SLURM_JOB_NUM_NODES            ## Number of nodes
-cores=$SLURM_CPUS_ON_NODE             ## Number of cores
+# Time limit hrs:mm:ss         
+#SBATCH --time=12:00:00
+
+# Number of nodes                
+#SBATCH --nodes=2           
+
+# Number of tasks per per node
+#SBATCH --ntasks-per-node=24 
+
+# Que/Partition for submission
+#SBATCH --partition=24hour             
+
+# Error log
+#SBATCH --error=err_%job-name.log
+
+# Output log
+#SBATCH --output=out_%job-name.log
+
+# E-mail for notifications
+#SBATCH --mail-user=r.t.b.ranaweera@northumbria.ac.uk
+
+# Types of notification for e-mails
+#SBATCH --mail-type=<ALL>
+
+# Job que to submit
+#SBATCH --partition=que                
+# ###################################
+
+# Get number of nodes
+nodes=$SLURM_JOB_NUM_NODES
+# Get number of cores            
+cores=$SLURM_CPUS_ON_NODE             
 
 # Load Modules and setup
-## Commented out due to these being included in .bashrc file for load at login.
-## module purge  ## Remove all modules
-## module add openmpi/intel-opa/gcc-hfi/64/1.10.4  ## Load dedicated MPI module
-## module add gcc/5.2.0 ## Load GCC module
-## module add slurm/15.08.6 ## Load the SLURM module
-## module add ANSYS/18.0  ## Load the ANSYS module
+# Commented out due to these being included in .bashrc file for load at login.
+# module purge  ## Remove all modules
+module add openmpi/intel-opa/gcc-hfi/64/1.10.4  ## Load dedicated MPI module
+# Load GCC module
+# module add gcc/5.2.0
+# Load the SLURM module
+module add slurm/15.08.6
+# Load the ANSYS module
+module add ANSYS/18.0
 
 # Case Details
-JOURNALFILE= ## fluent.journal  ## Journal file name
+# JOURNALFILE= ## fluent.journal  ## Journal file name
 FLUENT = ## /ansys_inc/v190/fluent/bin/fluent ## Module path
 
 # Execute Solver
