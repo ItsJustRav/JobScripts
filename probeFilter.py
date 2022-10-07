cc5d5ffbf011e7475ef6ea1a4c9e02d6ad8850da
Ravindu <r.t.b.ranaweera@northumbria.ac.uk>
2022-10-07 10:31:14 +0100

    working plots with pandas
    need to clean-up and get on single graph.

--- probeFilter.py
+++ probeFilter.py
@@ -3,14 +3,23 @@ import matplotlib.pyplot as plt
 
 results_filename = "extracted_results.txt"
 
-df = pd.read_csv(results_filename, sep=',')
+df = pd.read_csv(results_filename, sep=',', dtype='float64')
+
+
+#df = df['nodenumber'].astype('float64')
 
 #print(df.dtypes)
 #print(df.info(verbose=True))
+#print(df.head())
+#list(df.columns)
 
-df2 = df[df['nodenumber']==22]
+df2 = df[df['nodenumber']==1]
 
+print(df2)
+#print(df2['pressure'])
 
-print(df2['pressure'])
+df.plot.scatter(x="nodenumber", y ='pressure', s=10, marker='x', yerr = 0, xerr = 0, title="Node No. vs Pressure", xlabel="Node No.", ylabel="Pressure (Pa)", legend=True)
+df2.plot.scatter(x="nodenumber", y='pressure-coefficient', s=10, marker='x', yerr = 0, xerr = 0, title="Node No. vs Pressure", xlabel="Node No.", ylabel="Pressure (Pa)", legend=True)
+#plt.legend(loc='upper left', frameon=False)
 
-plt.axes(df2['pressure'])
+plt.show()
